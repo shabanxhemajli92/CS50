aaea091a26b279e2686c0ebece89dbcc93aeeb0d
Shaban <shabanxhemajli@live.com>
2022-09-23 16:03:14 +0200

    final version

--- loops.py
+++ loops.py
@@ -1,3 +1,23 @@
-names=["John","Jim","Alvin"]
-for name in names:
-    print(name)
+# names=["John","Jim","Alvin"]
+# for name in names:
+#     print(name)
+
+def full_name(*args):
+    
+    full_string = ""
+    for name in args:
+        full_string += f"{name} " 
+    return full_string
+
+
+print(full_name("Felipe", "Gonzales", "Garcia", "Gonzales", "Garcia", "Gonzales", "Garcia", "Gonzales", "Garcia", "Gonzales", "Garcia", "Gonzales", "Garcia", "Gonzales", "Garcia"))
+print(full_name("Gonzales", "Garcia"))
+print(full_name("Gonzales")) 
+
+  
+    
+
+
+
+
+     
