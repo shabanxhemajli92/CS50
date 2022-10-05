62b788ff7f8f2dcb66ae727aa7475cc4fe2f86cc
Shaban <shabanxhemajli@live.com>
2022-10-05 09:51:58 +0200

    Some loop work

---
+++ git-intro/keys_values.py
@@ -0,0 +1,13 @@
+fruit_colors = {
+    "apple": "red",
+    "berries": "blue"
+}
+
+for color in fruit_colors:
+    print(color)
+
+for items in fruit_colors.items():
+    print(items)
+
+for values in fruit_colors.values():
+    print(values)
