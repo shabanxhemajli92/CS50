4fe57e2ff1d98370e25c72a7e65ec860ad2276cc
Shaban <shabanxhemajli@live.com>
2022-10-08 13:25:19 +0200

    lists

--- lists.py
+++ lists.py
@@ -3,10 +3,16 @@
 # a.append("hello")
 # a.pop()
 # a[0]=100
-b=["banana","apple","microsoft"]
-temp=b[0]
-b[0]=b[2]
-b[2]=temp
-print(b)
-b[1],b[0]=b[0],b[1]
-print(b)
+# b=["banana","apple","microsoft"]
+# temp=b[0]
+# b[0]=b[2]
+# b[2]=temp
+# print(b)
+# b[1],b[0]=b[0],b[1]
+# print(b)
+# numb=[1,2,3,4,5,6,7,8,9,10]
+# my_list= []
+# for n in numb:
+#     my_list.append(n)
+#     print(my_list)
+
