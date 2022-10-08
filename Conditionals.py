152d0a44f241d197772d744837ac7a16478e6573
Shaban <shabanxhemajli@live.com>
2022-10-08 12:28:35 +0200

    conditionals work

--- Conditionals.py
+++ Conditionals.py
@@ -1,12 +1,26 @@
-score=int(input("Score: "))
+# score=int(input("Score: "))
 
-if score >= 90:
-    print("A")
-elif score >= 80:
-    print("B")  
-elif score >=70:
-    print("C") 
-elif  score >=60:
-    print("D")
-else:
-    print("F")           
+# if score >= 90:
+#     print("A")
+# elif score >= 80:
+#     print("B")  
+# elif score >= 70:
+#     print("C") 
+# elif  score >= 60:
+#     print("D")
+# else:
+#     print("F")  
+ 
+def main():
+    x = int(input("whats x?"))
+    if is_even(x):
+        print("Even")
+    else:
+        print("odd")
+
+def is_even(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False    
+main()
