d2e68efcf362a47d06af72caf21df8c56c3c6e3d
Shaban <shabanxhemajli@live.com>
2022-10-29 15:34:53 +0200

    some class fun

---
+++ git-intro/teamclass.py
@@ -0,0 +1,28 @@
+class Team:
+
+    def __init__(self,players,positions,numbers,form):
+        self.players=players
+        self.positions=positions
+        self.numbers=numbers
+        self.form=form
+
+    
+def what_is_form(self):
+        print(f"{self.plyer} is in form")
+
+def how_many_goals(self):
+        print(f"{self.position}has scored 3 goals")
+
+def what_is_thenumber():
+        print("is their number")
+
+player1=Team("Joaquin","Striker",25,"can be better")
+player2=Team("Alberto","Striker",21,"amazing for a sub")
+player3=Team("Michele","Striker",22,"very bad")
+print(player3.form)
+
+
+
+
+
+
